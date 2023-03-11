f1f39b9e7a7b15a3a9d73e6a9d5ec037d0bd4cf4
PracticeKJY <poiijtl1004@naver.com>
2023-03-11 18:26:49 +0900

    src폴더 안에 assets 생성

--- src/components/Header/HeaderIcon/HeaderIcon.jsx
+++ src/components/Header/HeaderIcon/HeaderIcon.jsx
@@ -1,7 +1,9 @@
 import styles from './HeaderIcons.module.css'
-import Location from '../../../../public/assets/icons/Icon/header-location.svg'
+// import Location from '../../../../public/assets/icons/Icon/header-location.svg'
+import Location from '@/assets/icons/Icon/header-location.svg'
 import Heart from '../../../../public/assets/icons/Icon/header-heart.svg'
-import Cart from '../../../../public/assets/icons/Icon/header-Cart.svg'
+import Cart from '@/assets/icons/Icon/header-Cart.svg'
+
 export function HeaderIcon() {
   return (
     <div className={styles.mainIcons}>
