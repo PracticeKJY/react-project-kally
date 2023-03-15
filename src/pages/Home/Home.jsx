64b9f9305c21617065942ab25254eb185ae17629
PracticeKJY <poiijtl1004@naver.com>
2023-03-16 00:51:07 +0900

    중간저장용커밋

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,39 @@
-import { useState, useLayoutEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Footer } from '../../components/Footer/Footer';
 
 import { Mainmodal } from './Mainmodal/Mainmodal';
 
+let renderCount = 1;
+
 function Home() {
   // isModalOpen은 메인모달창이 떠있는 여부를 결정한다
-  const [isModalOpen, setModalOpen] = useState(true);
+  const [isCheckModalOpen, setCheckModalOpen] = useState(true);
 
   // 사용자에게 노출되는 DOM을 변형시킬 때 효과적인 useLayoutEffect
-  useLayoutEffect(() => {
+  useEffect(() => {
+    // console.log('테스트용 로그');
+
     // getCookie 함수를 통해 'modalClose'라는 쿠키가 있는지 확인한다
     let modalCloseValue = getCookie('modalClose');
 
     if (modalCloseValue && modalCloseValue?.includes('T')) {
-      // 쿠키가 존재할 때
-      setModalOpen(false); // 모달창을 띄우지 않는다
+      setCheckModalOpen(false); //리랜더링 감지 (감지 후 비동기 처리 - 바로 리랜더링을 시도하지 않음 ) (모달오픈이라는 상태를 변경한다고해서 리랜더링이 아님)
     }
-    console.log(modalCloseValue);
-  }, []); // 첫 랜더링에만 동작
-  console.log(isModalOpen);
+
+    console.log('Effect Callback', modalCloseValue); // 2 (T)
+  }); //
+  console.log(
+    renderCount++ === 1 ? 'rendering' : 'reRendering',
+    isCheckModalOpen,
+    renderCount
+  ); // 1. (true) 3. (false)
+  //새로고침 != 리랜더링  , 새로고침은 맨처음랜더링 부터 다시시작하는거다
 
   return (
     <>
       <div>Home</div>
-      {isModalOpen ? <Mainmodal setModalOpen={setModalOpen} /> : null}
+      {isCheckModalOpen ? <Mainmodal setModalOpen={setCheckModalOpen} /> : null}
     </>
   );
 }
